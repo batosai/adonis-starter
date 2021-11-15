0e4192068a5223511df7796a22d8c74baeaae0b6
jeremy <jeremy@chaufourier.fr>
2021-11-15 23:33:44 +0100

    Update @tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -203,7 +203,7 @@ module.exports = {
   },
   plugins: [
     require('tailwindcss-multi-theme'),
-    require('@tailwindcss/custom-forms'),
+    require('@tailwindcss/forms'),
     plugin(({ addUtilities, e, theme, variants }) => {
       const newUtilities = {}
       Object.entries(theme('colors')).map(([name, value]) => {
