eed53fd727debfd8f1900917fb3353be3023327f
jeremy <jeremy@chaufourier.fr>
2021-11-18 23:56:46 +0100

    Init maildev for development env

--- providers/AppProvider.ts
+++ providers/AppProvider.ts
@@ -12,7 +12,9 @@ export default class AppProvider {
   }
 
   public async ready() {
-    // App is ready
+    if (this.app.environment === 'web' && this.app.nodeEnvironment == 'development') {
+      await import('../start/maildev')
+    }
   }
 
   public async shutdown() {
---
+++ start/maildev.ts
@@ -0,0 +1,11 @@
+import MailDev from 'maildev'
+
+
+const maildev = new MailDev({
+  // basePathname: '/maildev'
+})
+
+// Maildev now running on localhost:1080/maildev
+maildev.listen(function (err) {
+  console.log('We can now sent emails to port 1025!')
+})
